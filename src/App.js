e60ee47ecd3bea1926735df4ea7b587a77d683c2
SaraD22 <91880957+SaraD22@users.noreply.github.com>
2022-04-27 15:14:15 +0200

    Added GitHub link

--- src/App.js
+++ src/App.js
@@ -2,5 +2,15 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was created by Sara Duarte and is{" "}
+        <a href="https://github.com/SaraD22/shecodes_weather">
+          open-sourced on GitHub.
+        </a>
+      </footer>
+    </div>
+  );
 }
