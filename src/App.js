4e4720be85eee6ddcf0393f858091edd109d9264
SaraD22 <91880957+SaraD22@users.noreply.github.com>
2022-04-27 15:23:47 +0200

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React from "react";
+import "bootstrap/dist/css/bootstrap.css";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was created by Sara Duarte and is{" "}
-        <a href="https://github.com/SaraD22/shecodes_weather">
-          open-sourced on GitHub.
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was created by Sara Duarte and is{" "}
+          <a href="https://github.com/SaraD22/shecodes_weather">
+            open-sourced on GitHub.
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
